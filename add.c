e8dd2a872f156b59ec38e9b6dd4caeeaff7e7784
surender kumar <surenderjnv@gmail.com>
2021-06-30 16:23:08 +0530

    Subtraction function added

--- add.c
+++ add.c
@@ -4,6 +4,10 @@ int add(int a, int b){
 	return a+b;
 }
 
+int sub(int a, int b){
+	return a-b;
+}
+
 int main(){
 	printf("Hello World!\n");
 	int a=2, b=3;
@@ -12,8 +16,11 @@ int main(){
 	scanf("%d%d", &a, &b);
 
 	int sum = add(a, b);
+	int diff = sub(a, b);
 
 	printf("Sum of %d and %d is : %d\n",a, b, sum);
+
+	printf("Subtraction of %d and %d is : %d\n", a, b, diff);
 
 	
 	return 0;
